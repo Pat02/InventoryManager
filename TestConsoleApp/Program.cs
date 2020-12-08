fa131a353dce2811a69600a178a462f4961c09fe
Patrick Eldridge <pweldridge@gmail.com>
2020-12-07 20:31:33 -0500

    updated test console app to test dataservice.

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using InventoryManager.DnD5eAPI;
+using InventoryManager.Domain.Models;
+using InventoryManager;
+using InventoryManager.EntityFramework;
+using InventoryManager.EntityFramework.Services;
 using System.Collections.Generic;
 using InventoryManager.DnD5eAPI.Results;
 using InventoryManager.DnD5eAPI.Services;
@@ -7,6 +11,8 @@ using Microsoft.VisualBasic;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using InventoryManager.DnD5eAPI.Models;
+using InventoryManager.Domain.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestConsoleApp
 {
@@ -16,14 +22,24 @@ namespace TestConsoleApp
 
         static async Task Main(string[] args)
         {
-            ItemListService itemList = new ItemListService();
-            ItemService itemService = new ItemService();
-            var result = await itemList.ItemList("/api/equipment");
-            var result2 = await itemService.GetItem(@"/api/equipment/backpack");
-            foreach (EquipmentListLineItem thing in result.results)
+            DbContextOptionsBuilder options = new DbContextOptionsBuilder<InventoryManagerDbContext>();
+            options.UseSqlite(@"Data Source=InventoryManager.db;");
+            using (InventoryManagerDbContext context = new InventoryManagerDbContext(options.Options))
             {
-                Console.WriteLine($"{thing.name} : {thing.index}");
+                context.Database.Migrate();
             }
+            Action<DbContextOptionsBuilder> configureDbContext = o => o.UseSqlite(@"Data Source=InventoryManager.db;");
+
+            InventoryManagerDbContextFactory contextFactory = new InventoryManagerDbContextFactory(configureDbContext);
+            IDataService<Item> dataService = new GenericDataService<Item>(contextFactory);
+            Guid testGuid = new Guid();
+            Item item = new Item() {id = testGuid, Name = "Test Item of Valor", Weight = 1 };
+
+            var i = await dataService.Create(item);
+
+            var retrievedItem = await dataService.Get(testGuid);
+            Console.WriteLine($"Name: {retrievedItem.Name} \n Weight: {retrievedItem.Weight}");
+
         }
     }
 }
