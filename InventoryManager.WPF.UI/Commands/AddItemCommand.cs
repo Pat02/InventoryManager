ee32112cf266ecb713e692bfbfc919ae21851451
Patrick Eldridge <pweldridge@gmail.com>
2021-01-02 19:43:25 -0500

    Added Commands and Command Folder

---
+++ InventoryManager.WPF.UI/Commands/AddItemCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace InventoryManager.WPF.UI.Commands
+{
+    public class AddItemCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
